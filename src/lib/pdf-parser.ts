7f918a73ac6c514047539892599fb73e2f333de4
Jack Sheehy <jackhughsheehy@gmail.com>
2024-12-27 21:07:24 +0000

    Remove bad imports ! + new pdf2json method

--- src/lib/pdf-parser.ts
+++ src/lib/pdf-parser.ts
@@ -1,13 +1,36 @@
 import PDFParser from 'pdf2json'
 
+interface PdfParserError {
+  parserError: Error
+}
+
+interface PdfChunk {
+  T: string
+}
+
+interface PdfText {
+  R: PdfChunk[]
+}
+
+interface PdfPage {
+  Texts: PdfText[]
+}
+
+interface PdfData {
+  Pages: PdfPage[]
+}
+
 export async function parsePdf(buffer: ArrayBuffer): Promise<string> {
   return new Promise((resolve, reject) => {
     const pdfParser = new PDFParser()
 
-    pdfParser.on('pdfParser_dataError', (errData: any) => reject(errData.parserError))
-    pdfParser.on('pdfParser_dataReady', (pdfData: any) => {
-      const text = pdfData.Pages.map((page: any) =>
-        page.Texts.map((text: any) => decodeURIComponent(text.R[0].T)).join(' ')
+    pdfParser.on('pdfParser_dataError', (errData: PdfParserError) => {
+      reject(errData.parserError)
+    })
+
+    pdfParser.on('pdfParser_dataReady', (pdfData: PdfData) => {
+      const text = pdfData.Pages.map((page) =>
+        page.Texts.map((txt) => decodeURIComponent(txt.R[0].T)).join(' ')
       ).join('\n')
       resolve(text)
     })
@@ -15,4 +38,3 @@ export async function parsePdf(buffer: ArrayBuffer): Promise<string> {
     pdfParser.parseBuffer(Buffer.from(buffer))
   })
 }
-
